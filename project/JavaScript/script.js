f3f517280ee60b91f6fe1fa2f06f5575ade9f884
RowaydaMohamed <Roweda_20230218@fci.helwan.edu.eg>
2024-04-30 19:51:57 +0300

    turt

--- project/JavaScript/script.js
+++ project/JavaScript/script.js
@@ -175,4 +175,29 @@ document.addEventListener("DOMContentLoaded", function() {
   }, 3000);
 });
 
+//oct salma
+var image = document.getElementById('image1');
+var image1Path = '../Images/Oct3.jpg';
+var image2Path = '../Images/Oct2.png';
+var originalSrc = image.src;
+
+
+image.addEventListener('mouseover', function() {
+  // Change the source of the image to image2.jpg on mouseover
+  image.src = image2Path;
+});
+
+image.addEventListener('mouseout', function() {
+  // Change the source back to the original source on mouseout
+  image.src = originalSrc;
+});
+
+
+
+document.getElementById('playButton').addEventListener('click', function() {
+  var audio = document.getElementById('audiooct');
+  audio.play();
+});
+
+
 
