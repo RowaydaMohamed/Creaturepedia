c089431feecf05ce6c11af9fed7ee09b4fd6bd24
RowaydaMohamed <Roweda_20230218@fci.helwan.edu.eg>
2024-04-30 19:13:00 +0300

    .

--- project/JavaScript/script.js
+++ project/JavaScript/script.js
@@ -16,9 +16,9 @@ audioButton.addEventListener('click', function() {
     audio.pause();
   }
 });
-function flipCard(card) {
-    card.classList.toggle('flipped');
-  }
+// function flipCard(card) {
+//     card.classList.toggle('flipped');
+//   }
   
 // JavaScript logic to change background image
 const backgrounds = ['../Images/orcabackground3.jpeg', '../Images/sea_background.png'];
@@ -112,24 +112,6 @@ function mOver(obj) {
     return true;
 }
 
-//elephant
-let currentIndex = 0;
-const images = document.querySelectorAll('.image-box img');
-
-function showNextImage() {
-  images[currentIndex].classList.remove('active');
-  currentIndex = (currentIndex + 1) % images.length;
-  images[currentIndex].classList.add('active');
-}
-
-// Show the first image when the page loads
-images[currentIndex].classList.add('active');
-
-// Find the button element
-const button = document.querySelector('button');
-
-// Add a click event listener to the button
-button.addEventListener('click', showNextImage);
 
 // salma parrot2.js
 document.addEventListener("DOMContentLoaded", function() {
